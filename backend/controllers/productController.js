d8a23a092dc7be37ababd1f66e68652948103f6c
Tsiory Mialinarivo <tsiorymanana7@gmail.com>
2023-08-05 16:56:46 +0300

    Setting up Redux store

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,17 @@
 import Product from "../models/productModel.js"
 import handleAsync from "../utils/handleAsync.js"
 
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 const getProducts = handleAsync(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
 })
 
+// @desc    Fetch a product
+// @route   GET /api/products/:id
+// @access  Public
 const getProductById = handleAsync(async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
