e1c4bc5bdafc6f05c1f9c789e6452d3025c9b3f5
Tsiory Mialinarivo <tsiorymanana7@gmail.com>
2023-09-04 17:24:39 +0300

    fix server

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const __dirname = path.resolve()
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 // For production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "development") {
     app.use(express.static(path.join(__dirname, "frontend/build")))
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
